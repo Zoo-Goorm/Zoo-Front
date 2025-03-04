5aa67de723dbdbcf8903b348c5908900df73d9eb
sohee <106158901+ccconac@users.noreply.github.com>
2025-03-04 15:48:31 +0900

    chore: 디자인 시스템 정의 (color token/typography) (#3)
    * remove: package-lock.json 파일 삭제
    
    * chore: tailwind 버전 downgrade 및 환경 설정 수정
    
    * chore: design system color token 정의
    
    * rename: postcss.config 확장명 변경 (ts → mjs)
    
    * chore: design system typography 정의

--- zoo-client/src/app/layout.tsx
+++ zoo-client/src/app/layout.tsx
@@ -4,16 +4,6 @@ import Providers from './utils/providers'
 import './globals.css'
 import { IChildren } from '@/types/children'
 
-const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin'],
-})
-
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin'],
-})
-
 export const metadata: Metadata = {
   title: 'Zoo',
   description: 'Zoo client',
@@ -22,9 +12,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: Readonly<IChildren>) {
   return (
     <html lang="ko">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body>
         <Providers>{children}</Providers>
       </body>
     </html>
