3c95f050b9b7a3a8cf4d1c555ca76ba2e2a51e15
leesuyeon <lgh0385hh@gmail.com>
2025-03-06 11:43:57 +0900

    feat: Tab 컴포넌트 구조 생성

--- zoo-client/src/components/tab.tsx
+++ zoo-client/src/components/tab.tsx
@@ -3,22 +3,39 @@
 type TabDatesProps = {
   text: string
   selected: boolean
+  className?: string
+}
+
+function TabDates({ text, selected, className = '' }: TabDatesProps) {
+  return (
+    <span
+      className={`cursor-pointer font-semibold sm:text-sm lg:text-xl ${
+        selected ? 'text-purple-500' : 'text-[#898989]'
+      } ${className}`}
+    >
+      {text}
+    </span>
+  )
 }
 
 export default function Tab() {
   return (
-    <>
-      <TabDates text="전체" selected={true} />
-    </>
-  )
-}
+    <div className="flex w-full min-w-[312px] items-center justify-between bg-[#F9F9F9] px-[20px] py-[16px] max-sm:h-[41px] max-sm:px-[12px] sm:py-[12px] md:h-[61px]">
+      <div className="flex items-center gap-1 md:gap-4">
+        <TabDates className="hidden md:block" text="전체" selected={false} />
+        <span className="text-medium hidden text-[#d1d1d1] sm:text-sm md:block md:text-xl">
+          |
+        </span>
+        <TabDates text="N월 N일 (Day N)" selected={false} />
+        <span className="text-medium text-[#d1d1d1] sm:text-sm md:text-xl">
+          |
+        </span>
+        <TabDates text="N월 N일 (Day N)" selected={false} />
+      </div>
 
-function TabDates({ text, selected }: TabDatesProps) {
-  return (
-    <span
-      className={`text-semibold sm:text-sm md:text-xl ${selected ? 'text-purple-500' : 'text-grey-n-400'}`}
-    >
-      {text}
-    </span>
+      <div className="hidden max-sm:flex">
+        <span>hi</span>
+      </div>
+    </div>
   )
 }
