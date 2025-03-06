c51ba6cfcb436f69f80d89fa350b018c2c80adf2
leesuyeon <lgh0385hh@gmail.com>
2025-03-06 23:13:34 +0900

    fix: page 내부 코드 제거

--- zoo-client/src/app/page.tsx
+++ zoo-client/src/app/page.tsx
@@ -1,8 +1,5 @@
-import { DateInfoAccordion } from '@/app/components/index';
 export default function Home() {
   return (
-    <div className="flex h-screen flex-col items-center justify-center">
-      <DateInfoAccordion date={'3월 10일 (DAY N)'} />
-    </div>
+    <div className="flex h-screen flex-col items-center justify-center"></div>
   );
 }
