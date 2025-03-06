4c4c9576ff450a74438c0afd1ab8cf07a3d7ec8f
ccconac <qwerpoiu91@naver.com>
2025-03-06 20:02:56 +0900

    feat: badge list component 구현

--- zoo-client/src/types/badge/Badge.ts
+++ zoo-client/src/types/badge/Badge.ts
@@ -4,3 +4,12 @@ export interface IBadgeProps {
   text: string;
   type: TBadgeType;
 }
+
+export interface Badge {
+  name: string;
+  type: TBadgeType;
+}
+
+export interface IBadgeListProps {
+  badgeList: Badge[];
+}
