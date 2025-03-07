4cf1ffb1b79db687c6716c50f6809ecceb2b0938
ccconac <qwerpoiu91@naver.com>
2025-03-07 12:21:10 +0900

    feat: oblong button component 구현

--- zoo-client/src/components/index.ts
+++ zoo-client/src/components/index.ts
@@ -5,3 +5,6 @@ export { default as SessionContent } from './common/session/SessionContent';
 export { default as Session } from './common/session/Session';
 export { default as Accordion } from './common/accordion/Accordion';
 export { default as DateInfoAccordion } from './common/accordion/DateInfoAccordion';
+
+/* button */
+export { default as OblongButton } from './common/button/OblongButton';
