7336fe5a5b632d584bc6296c39f9cfbd32ac3e87
ccconac <qwerpoiu91@naver.com>
2025-03-07 15:32:15 +0900

    feat: session component hover 시 list button component 표시

--- zoo-client/src/components/common/session/Session.tsx
+++ zoo-client/src/components/common/session/Session.tsx
@@ -1,22 +1,35 @@
+'use client';
+
+import { useState } from 'react';
+
 import SessionContent from './SessionContent';
 import SpeakerList from '../speaker/SpeakerList';
-import { ISpeakerList } from '@/types/speaker/speaker';
+import ApplyButton from './ApplyButton';
+
 import { IBadgeList } from '@/types/badge/Badge';
+import { ISpeakerList } from '@/types/speaker/speaker';
 
 export default function Session({
   badgeList,
   speakerList,
 }: ISpeakerList & IBadgeList) {
+  const [isHovered, setIsHovered] = useState(false);
+
   return (
-    <div className="flex max-w-[64.375rem] items-center gap-5">
+    <div
+      className="session-container flex max-w-[64.375rem] items-center gap-5"
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+    >
       <div className="flex flex-1 items-center gap-5 self-stretch">
         <SessionContent
           badgeList={badgeList}
           sessionTitle="제목은 최대한 짧게 가자 하지만 길어진다면 2줄처리가 되게 콘텐츠 팀에서 연사님이랑 협의봐주세요 제발료"
           sessionBody="세션 상세 설명"
         />
+        <SpeakerList speakerList={speakerList} />
       </div>
-      <SpeakerList speakerList={speakerList} />
+      {isHovered && <ApplyButton />}
     </div>
   );
 }
