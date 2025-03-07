519b435b76ad3d7eab39a10600024f49a4420a74
Suyeon Lee <122202949+leeluse@users.noreply.github.com>
2025-03-07 16:43:28 +0900

    feat: progressbar component 구현 (#22)

--- zoo-client/src/app/page.tsx
+++ zoo-client/src/app/page.tsx
@@ -1,12 +1,5 @@
-import { Radio } from '@/components';
-
 export default function Home() {
   return (
-    <div className="flex h-screen flex-col items-center justify-center bg-bg-primary">
-      <Radio
-        status="active"
-        video="https://www.youtube.com/watch?v=6JYIGclVQdw"
-      />
-    </div>
+    <div className="flex h-screen flex-col items-center justify-center bg-bg-primary"></div>
   );
 }
