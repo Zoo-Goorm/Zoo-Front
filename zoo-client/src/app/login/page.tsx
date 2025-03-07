a1f9ee9496c3f8eaa3b6247cdb601defab0b0a14
sohee <106158901+ccconac@users.noreply.github.com>
2025-03-07 19:00:32 +0900

    feat: 로그인 페이지 UI 구현 (#24)
    * feat: 임시 navigation bar component 구현
    
    * design: login button website 너비 변경
    
    * chore: 텍스트 전역 스타일 추가
    
    * feat: 로그인 페이지 UI 구현

---
+++ zoo-client/src/app/login/page.tsx
@@ -0,0 +1,25 @@
+import { LoginButton, NavigationBar } from '@/components';
+import { LOGIN_PAGE_MESSAGE } from '@/constants/messages';
+
+export default function Login() {
+  return (
+    <div className="flex h-[100vh] w-[100vw] flex-col items-center gap-[0.4375rem] bg-bg-primary pb-[13.4375rem]">
+      <NavigationBar />
+      <div className="flex h-[100%] w-[100%] max-w-[25rem] flex-col items-center justify-center gap-40">
+        <div className="flex flex-col items-start gap-24 self-stretch">
+          <h2 className="display-b-48 self-stretch text-text-main">
+            {LOGIN_PAGE_MESSAGE.loginTitle}
+          </h2>
+          <p className="body-sb-20 text-text-sub">
+            {LOGIN_PAGE_MESSAGE.loginBody}
+          </p>
+        </div>
+        <div className="flex flex-col items-start gap-16 self-stretch">
+          <LoginButton type="kakao" />
+          <LoginButton type="google" />
+          <LoginButton type="nonMember" />
+        </div>
+      </div>
+    </div>
+  );
+}
--- zoo-client/src/components/index.ts
+++ zoo-client/src/components/index.ts
@@ -15,3 +15,5 @@ export { default as OblongButton } from './common/button/OblongButton';
 export { default as ListButton } from './common/button/ListButton';
 export { default as TopButton } from './common/button/TopButton';
 export { default as LoginButton } from './common/button/LoginButton';
+
+export { default as NavigationBar } from './common/navigationBar/NavigationBar';
