e2f2ac66111c46118109fde556011cfb6b54ce6c
leesuyeon <lgh0385hh@gmail.com>
2025-03-10 10:27:17 +0900

    fix: session context 추가

--- zoo-client/src/app/session-schedule/page.tsx
+++ zoo-client/src/app/session-schedule/page.tsx
@@ -6,6 +6,7 @@ import {
   Tab,
   SessionList,
 } from '@/components';
+import { SESSION_SCHEDULE_MESSAGES } from '@/constants/session';
 
 import { subjectList } from '@/mock/chip';
 
@@ -15,9 +16,7 @@ const Title = () => {
       <h1 className="display-b-60 leading-none text-text-main">전체 세션</h1>
       <div className="flex flex-col gap-24">
         <p className="title-sb-24 leading-normal text-text-sub">
-          컨퍼런스 참가권 구매 시 모든 세션이 신청이 가능합니다.
-          <br />* 같은 시간대 세션은 중복 신청이 불가하오니 이점 확인해 주시길
-          바랍니다.
+          {SESSION_SCHEDULE_MESSAGES.title}
         </p>
         <PurchaseButton />
       </div>
