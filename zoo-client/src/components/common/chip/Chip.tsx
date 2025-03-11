7a8daa9716b53b639de14a07fa9fcccf46be70ea
ccconac <qwerpoiu91@naver.com>
2025-03-11 20:04:31 +0900

    feat: chip component에 x-box icon 추가

--- zoo-client/src/components/common/chip/Chip.tsx
+++ zoo-client/src/components/common/chip/Chip.tsx
@@ -1,21 +1,31 @@
 'use client';
 
 import { useChipStore } from '@/store/useChipStore';
+import Image from 'next/image';
 
 interface IChipProps {
+  size: 'l' | 's';
   text: string;
 }
 
-export default function Chip({ text }: IChipProps) {
+export default function Chip({ text, size }: IChipProps) {
   const { selectedChips, toggleChip } = useChipStore();
   const isSelected = selectedChips.has(text);
 
+  const chipSizeClasses = {
+    l: 'px-8 py-4 rounded-sm',
+    s: 'p-4 rounded-[0.25rem] text-text-main caption-r-12',
+  };
+
   return (
     <div
-      className={`inline-flex items-center justify-center gap-0 rounded-sm website:px-8 website:py-4 ${isSelected ? 'bg-fill-secondary' : 'bg-bg-thirary'}`}
+      className={`inline-flex items-center justify-center gap-0 ${chipSizeClasses[size]} ${isSelected ? 'bg-fill-secondary' : 'bg-bg-thirary'}`}
       onClick={() => toggleChip(text)}
     >
       {text}
+      {size === 's' && (
+        <Image src="/chip/xbox.svg" alt="chip x-box" width={16} height={16} />
+      )}
     </div>
   );
 }
