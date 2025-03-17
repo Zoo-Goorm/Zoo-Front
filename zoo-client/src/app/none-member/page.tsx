922ea0b7e227c5942a2dc99638e9dec32b5a311e
ccconac <qwerpoiu91@naver.com>
2025-03-17 16:59:39 +0900

    design: 비회원 인증 페이지 레이아웃 디자인 수정
    h-[100vh] 제거

--- zoo-client/src/app/none-member/page.tsx
+++ zoo-client/src/app/none-member/page.tsx
@@ -2,7 +2,7 @@ import { Footer, NavigationBar, NoneMemberForm } from '@/components';
 
 export default function NoneMember() {
   return (
-    <div className="flex h-[100vh] w-[100vw] flex-col items-center gap-[6.25rem]">
+    <div className="flex w-[100vw] flex-col items-center gap-[6.25rem]">
       <NavigationBar />
       <NoneMemberForm />
       <Footer />
