dff795f31344192ff6918107d8558e2fa21932ef
ccconac <qwerpoiu91@naver.com>
2025-03-27 19:05:42 +0900

    feat: 비회원 로그인 API 요청 함수 구현

--- zoo-client/src/services/auth.ts
+++ zoo-client/src/services/auth.ts
@@ -29,3 +29,31 @@ export async function fetchEmailVerification(email: string) {
     body: JSON.stringify({ email }),
   });
 }
+
+export interface IUserInfo {
+  name: string;
+  email: string;
+  code: string;
+}
+
+export async function fetchNoneMemberLogin({ name, email, code }: IUserInfo) {
+  const endpoint = `${baseURL}/api/v1/user/login`;
+
+  const response = await fetch(endpoint, {
+    method: 'POST',
+    body: JSON.stringify({ name, email, code }),
+  });
+
+  if (response.status !== 200) {
+    throw new Error(`HTTP error status: ${response.status}`);
+  }
+
+  const authorizationHeader = response.headers.get('Authorization');
+
+  if (authorizationHeader) {
+    const token = authorizationHeader.split(' ')[1];
+    localStorage.setItem('noneMemberAccessToken', token);
+  }
+
+  return null;
+}
