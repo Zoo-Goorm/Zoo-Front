3ade3e68408b6fbb312c069575a130d981370308
leesuyeon <lgh0385hh@gmail.com>
2025-04-01 15:18:58 +0900

    test

--- zoo-client/src/store/common/insight/useSaveInsight.ts
+++ zoo-client/src/store/common/insight/useSaveInsight.ts
@@ -2,22 +2,15 @@ import { create } from 'zustand';
 
 interface SaveInsightState {
   content: string;
-  textHeight: string;
 }
 
 interface SaveInsightAction {
   updateInsight: (newContent: string) => void;
-  setTextHeight: (height: string) => void;
 }
 
 export const useSaveInsight = create<SaveInsightState & SaveInsightAction>(
   (set) => ({
     content: '',
-    textHeight: 'auto',
-    setTextHeight: (height) =>
-      set(() => ({
-        textHeight: height,
-      })),
     updateInsight: (newContent) =>
       set(() => ({
         content: newContent,
